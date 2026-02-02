083c6d00567d65c6c8e30b80665bbb3f7561bc5d
Zhong Lin <u3598858@connect.hku.hk>
2026-02-02 13:35:22 +0800

    Initial Commit

---
+++ Flowers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Flowers
+//
+//  Created by Zhong Lin on 2/2/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Flowers/FlowersApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowersApp.swift
+//  Flowers
+//
+//  Created by Zhong Lin on 2/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct FlowersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
